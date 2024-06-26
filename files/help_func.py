5c7ee2c7ecd602c6ad9c70c1a58e1c5bd32aaa6b
Nikita Kashin <nikita.kashin@red-soft.ru>
2024-06-26 17:53:25 +0300

    Transfer test_create_index.py. See RS-183618
    Also remove useless test

--- files/help_func.py
+++ files/help_func.py
@@ -1,5 +1,6 @@
+import os
+
 def get_path():
-    import os
     DIST = os.environ.get('DIST')
     ARCH = os.environ.get('ARCH')
     if DIST:
@@ -12,3 +13,25 @@ def get_path():
         path_to_exe = '"C:\\Program Files\\RedExpert\\bin\\RedExpert64.exe"'
 
     return path_to_exe
+
+def clear_history_files():
+    home_dir = os.path.expanduser("~")
+    if "ui-improvement" in os.environ["BRANCH"]:
+        build_no = "202406"
+    else:
+        build_no = "202301"
+
+    history_file = os.path.join(home_dir,f'.redexpert/{build_no}/ConnectionHistory.xml')
+    saved_conn_file = os.path.join(home_dir,f'.redexpert/{build_no}/savedconnections.xml')
+    if os.path.exists(saved_conn_file):
+        with open(saved_conn_file, 'r') as f:
+            context = f.read()
+        
+        if "</connection>" in context:
+            context = context[:context.find("</connection>")] + "</connection>\n\n</savedconnections>"
+        
+            with open(saved_conn_file, 'w') as f:
+                f.write(context)
+
+    if os.path.exists(history_file):
+        os.remove(history_file)
---
+++ tests/test_create_index.robot
@@ -0,0 +1,33 @@
+*** Settings ***
+Library    RemoteSwingLibrary
+Library    Process
+Library    Collections
+Resource    ../files/keywords.resource
+Test Setup       Setup before every tests
+Test Teardown    Teardown after every tests
+
+*** Test Cases ***
+test_1
+    Create Index
+    Check Box Should Be Enabled    Active
+
+test_2
+    Create Index
+    Select From Combo Box    1    EMPLOYEE
+    Push Button    5
+    Click On List Item    1    EMP_NO
+    ${i}=     Set Variable    0
+    FOR    ${i}     IN RANGE    5
+        Push Button    10
+    END
+    ${list}=    Get List Values    1
+    ${result}=    Create List    FIRST_NAME    LAST_NAME    PHONE_EXT    HIRE_DATE    JOB_CODE    EMP_NO    JOB_GRADE    JOB_COUNTRY    SALARY
+    Lists Should Be Equal    ${list}    ${result}
+
+*** Keywords ***
+Create Index
+    Open connection
+    Expand Tree Node    0    New Connection    
+    Select From Tree Node Popup Menu    0    New Connection|Indices (38)    Create index
+    Select Dialog    Create index
+    
