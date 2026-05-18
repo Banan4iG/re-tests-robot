4586b424d2808152edfe8ce909956556019ed025
Nikita Kashin <nikita.kashin@red-soft.ru>
2026-05-18 09:45:59 +0300

    Improve print who blocking file.

--- files/help_func.py
+++ files/help_func.py
@@ -178,19 +178,22 @@ def unlock_employee():
     delta_file = home_directory + "examples/empbuild/employee.fdb.delta"
     if os.path.exists(delta_file):
         time.sleep(2) 
-        print_file_blocker(delta_file)
+        list_proc = file_blocker(delta_file)
+        print(list_proc)
         os.remove(delta_file)
         subprocess.run([f"{home_directory}{bin_dir}nbackup{get_exe()}", "-F", f"{home_directory}examples/empbuild/employee.fdb"])
 
-def print_file_blocker(path: str):
+def file_blocker(path: str):
+    list_proc = []
     for proc in psutil.process_iter(['pid', 'name']):
         try:
             files = proc.open_files()
             for f in files:
                 if f.path == path:
-                    print(proc.info)
+                    list_proc.append(proc.info)
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             pass
+    return list_proc
 
 def execute(query: str):
     """
